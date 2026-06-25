671be210e8255961d612cc9c48b8ee828efa5340
Vikrant Singh <jadounvikrant@gmail.com>
2026-06-25 18:56:53 +0530

    wWorking on project 2

--- script.js
+++ script.js
@@ -2,15 +2,24 @@ var istatus = document.querySelector("h5");
 
 
 var addFriend = document.querySelector("#add")
-
-var remove = document.querySelector("#remove")
+var flag = 0;
 
 addFriend.addEventListener("click", function(){
+    if(flag==0){
     istatus.innerHTML ="Added to Friend list !"
     istatus.style.color ="green"
+    console.log("Click on add")
+    addFriend.textContent ="Remove";
+    flag =1;
+    }
+
+    else{
+        istatus.innerHTML ="Remove from List";
+        addFriend.textContent ="Add Friend";
+        istatus.style.color ="red";
+        console.log("Remove Friend!")
+        flag =0;
+    }
+    
 } )
 
-remove.addEventListener("click", function(){
-    istatus.innerHTML ="Remove from Friend list !"
-    istatus.style.color = "red";
-})
